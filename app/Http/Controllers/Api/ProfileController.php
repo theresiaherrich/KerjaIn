25bc066a995a9206ccbd85388e637c6134ec426d
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2025-03-13 15:51:58 +0700

    profile new

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -61,7 +61,7 @@ class ProfileController extends Controller
         }
     }
 
-    public function update(Request $request)
+    public function store(Request $request)
     {
         try {
             $user = auth('api')->user();
